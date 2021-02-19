823718d57f632ee36fd91d63822d36ab516ac280
fcar829 <fcar829@gmail.com>
2021-02-19 04:24:35 +0000

    updated questions array

--- candidate-testing.js
+++ candidate-testing.js
@@ -10,11 +10,11 @@ let candidateName = ''
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = '';
-let questions = ["Who was the first American woman in space?", 
+let questions = ["Who was the first American woman in space ?", 
 "True or false: 5000 meters == 5 kilometers ?",
 "(5 + 3)/2 * 10 = ?",
 "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?"
-, "What is the minimum crew size for the ISS?"];
+, "What is the minimum crew size for the ISS ?"];
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
 let candidateAnswers = [];
 
@@ -23,7 +23,7 @@ let candidateAnswers = [];
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
   console.log('')
-  candidateName = input.question("What is your name? ");
+  candidateName  = input.question("What is your name? ");
 }
 
 function askQuestion() {
