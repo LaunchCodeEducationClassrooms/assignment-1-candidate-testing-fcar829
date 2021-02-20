f4eaba37640a4b85a2387e562177432c83bf9cc8
fcar829 <fcar829@gmail.com>
2021-02-20 23:16:23 +0000

    updated questions

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,13 +8,13 @@ let candidateName = "";
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
-let questions = ['Who was the first American woman in space? ','True or false: 5000 meters == 5 kilometers? ', '(5 + 3)/2 * 10 =? ', "Given the array [8, 'orbit', 'Trajectory', 45], what entry is at index 2? ", 'What is the minimum crew size for the ISS? '];
-let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
+let questions = ['1) Who was the first American woman in space? ','2) True or false: 5000 meters == 5 kilometers? ', '3) (5 + 3)/2 * 10 =? ', "4) Given the array [8, 'orbit', 'Trajectory', 45], what entry is at index 2? ", '5) What is the minimum crew size for the ISS? '];
+let correctAnswers = ['Sally Ride', 'true', '40', 'Trajectory', '3'];
 let candidateAnswers = [];
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-candidateName = input.question("What is your name?: ");
+candidateName = input.question('Candidate Name: ');
 }
 
 function askQuestion() {
@@ -22,7 +22,7 @@ function askQuestion() {
 
  for (let i = 0; i < questions.length; i++) {
 candidateAnswers.push(input.question(questions[i]));
-console.log(`Your answer: ${candidateAnswers[i]}\nCorrect Answer: ${correctAnswers[i]}\n`);
+console.log(`Your Answer: ${candidateAnswers[i]}\nCorrect Answer: ${correctAnswers[i]}\n`);
  }
 }
 
